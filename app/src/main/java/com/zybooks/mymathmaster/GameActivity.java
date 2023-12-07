00556709b58fb99c551056ec2a677fc500490711
Talia-Lia <104951467+Talia-Lia@users.noreply.github.com>
2023-12-06 22:42:00 -0500

    Added the logic for the first two pages

--- app/src/main/java/com/zybooks/mymathmaster/GameActivity.java
+++ app/src/main/java/com/zybooks/mymathmaster/GameActivity.java
@@ -11,7 +11,6 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
-
     private Spinner chooseTimeSpinner;
     private RadioGroup difficultyRadioGroup;
     private Button startGameButton;
--- app/src/main/java/com/zybooks/mymathmaster/GameLogicActivity.java
+++ app/src/main/java/com/zybooks/mymathmaster/GameLogicActivity.java
@@ -1,4 +1,5 @@
 package com.zybooks.mymathmaster;
 
 public class GameLogicActivity {
+
 }
